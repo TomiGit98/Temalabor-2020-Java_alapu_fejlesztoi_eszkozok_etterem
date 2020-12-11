2afd2f61b60c51a8396f3e2f42620d7d75d5f9b4
Norman Zselinszky <zselinszky.norman@gmail.com>
2020-12-11 23:37:11 +0100

    renamed class

--- OnlineEtterem/src/test/java/PersonMockitoTest.java
+++ OnlineEtterem/src/test/java/PersonMockitoTest.java
@@ -13,7 +13,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
-public class PersonUnitTest {
+public class PersonMockitoTest {
 
     @InjectMocks
     PersonService personService;
